512c9af0a1327deef856f74be59a77dbe9b5e882
Nathan Van Gheem <vangheem@gmail.com>
2012-04-18 18:23:09 -0500

    index ocr data in portal catalog

---
+++ collective/documentviewer/catalog.py
@@ -0,0 +1,17 @@
+from Products.ATContentTypes.interface import IFileContent
+from plone.indexer import indexer
+from collective.documentviewer.settings import Settings
+
+
+@indexer(IFileContent)
+def SearchableText(object):
+    text = object.SearchableText()
+    if object.getLayout() != 'documentviewer':
+        return ''
+    settings = Settings(object)
+    catalog = settings.catalog
+    if catalog is not None:
+        index = catalog['text'].index
+        return [text, ' '.join(index._lexicon.words())]
+    else:
+        return text
