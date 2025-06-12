44031c1beed8b6faf30116278185301039048b13
Stephan Geulette <s.geulette@imio.be>
2025-06-12 15:44:20 +0200

    Improved javascript

--- collective/documentviewer/resources/javascripts/loader.js
+++ collective/documentviewer/resources/javascripts/loader.js
@@ -1,9 +1,12 @@
 
 var launchDV = function () {
     documentviewer = document.querySelector('.pat-documentviewer');
-    options = JSON.parse(documentviewer.getAttribute('data-pat-documentviewer'));
-    options.container = documentviewer;
-    DV.load(options.data, options);
+    if (documentviewer) {
+        options = JSON.parse(documentviewer.getAttribute('data-pat-documentviewer'));
+        console.log('IN LOADER with options:', options);
+        options.container = documentviewer;
+        DV.load(options.data, options);
+    }
 }
 
 document.addEventListener('DOMContentLoaded', launchDV);
