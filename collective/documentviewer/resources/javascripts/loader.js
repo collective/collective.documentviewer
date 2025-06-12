107923433a323d6bc4221d2a92dc6c49d7e3d6e7
Stephan Geulette <s.geulette@imio.be>
2025-06-12 15:57:48 +0200

    Removed console log

--- collective/documentviewer/resources/javascripts/loader.js
+++ collective/documentviewer/resources/javascripts/loader.js
@@ -3,7 +3,6 @@ var launchDV = function () {
     documentviewer = document.querySelector('.pat-documentviewer');
     if (documentviewer) {
         options = JSON.parse(documentviewer.getAttribute('data-pat-documentviewer'));
-        console.log('IN LOADER with options:', options);
         options.container = documentviewer;
         DV.load(options.data, options);
     }
